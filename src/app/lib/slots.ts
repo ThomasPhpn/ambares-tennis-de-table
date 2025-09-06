e6d33cfae60758d53bd0185f9c9047d7190e0a43
ThomasPhpn <tphilipponneau@gmail.com>
2025-09-06 16:04:14 +0200

    add créneaux

--- src/app/lib/slots.ts
+++ src/app/lib/slots.ts
@@ -11,23 +11,32 @@ export const SLOTS_BY_DAY: Record<string, SlotDef[]> = {
   monday: [
     { start: "17:00", end: "18:15", label: "Entraînement jeunes" },
     { start: "18:15", end: "19:30", label: "Entraînement jeunes" },
-    { start: "19:30", end: "21:00" }, // libre
+    { start: "19:30", end: "21:00" },
+    { start: "21:00", end: "22:00" },
   ],
   tuesday: [
+    { start: "17:00", end: "18:00" },
     { start: "18:00", end: "19:30", label: "Entraînement adultes" },
     { start: "19:30", end: "21:00", label: "Entraînement adultes" },
+    { start: "21:00", end: "22:00" },
   ],
-  wednesday: [{ start: "14:15", end: "16:00", label: "Entraînement jeunes" }],
+  wednesday: [
+    { start: "14:15", end: "16:00", label: "Entraînement jeunes" },
+    { start: "16:00", end: "17:30" },
+    { start: "17:30", end: "19:00" },
+    { start: "19:00", end: "20:30" },
+    { start: "20:30", end: "22:00" },
+  ],
   thursday: [
     { start: "17:00", end: "18:30" },
     { start: "18:30", end: "20:00" },
     { start: "20:00", end: "21:30" },
+    { start: "21:30", end: "22:00" },
   ],
   friday: [
     { start: "17:00", end: "18:30" },
-    { start: "18:30", end: "20:00" },
-    { start: "20:00", end: "21:30" },
-    { start: "20:30", end: "22:00", capacity: 28 }, // spécial 28 places
+    { start: "18:30", end: "20:30" }, // libre (14 places)
+    { start: "20:30", end: "22:00", capacity: 28 }, // spécial
   ],
   saturday: [
     { start: "10:00", end: "12:00" },
